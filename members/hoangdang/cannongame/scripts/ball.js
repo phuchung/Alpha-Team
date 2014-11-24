4118b3edcb051eebcb6fb1c05856a91b66d7acff
Hoàng Đặng <hoangdang89@users.noreply.github.com>
2014-11-24 15:41:50 +0700

    Game demo: cannon game, HTML5, canvas, part 1

---
+++ members/hoangdang/cannongame/scripts/ball.js
@@ -0,0 +1,32 @@
+/********************* ball ****************/
+function Ball(mapWidth, mapHeight, startX, startY, directionX, directionY, power) {
+	// the "life-zone"
+	this.minX = this.radius;
+	this.minY = this.radius;
+	this.maxX = mapWidth - this.radius;
+	this.maxY = mapHeight - this.radius;
+
+	this.speedX = directionX * power;
+	this.speedY = directionY * power;
+
+	this.cx = startX;
+	this.cy = startY;
+}
+
+Ball.prototype.draw = function (context) {
+
+	context.fillStyle = "black";
+	context.beginPath();
+	context.arc(this.cx, this.cy, 4, 0, Math.PI * 2, true);
+	context.closePath();
+	context.fill();
+}
+Ball.prototype.update = function () {
+	this.speedY += GRAVITY;
+	this.cx += this.speedX;
+	this.cy += this.speedY;
+	if (this.cx < this.minX || this.cx > this.maxX ||
+		this.cy < this.minY || this.cy > this.maxY)
+		return true;
+	return false;
+}
---
+++ members/hoangdang/cannongame/scripts/cannon.js
@@ -0,0 +1,75 @@
+function Cannon(mapWidth, mapHeight, x, y) {
+	this.mapWidth = mapWidth;
+	this.mapHeight = mapHeight;
+	this.radius = 10;
+	this.speed = 5;
+	this.power = 3;
+	this.cx = x;
+	this.cy = y;
+	this.angle = 0;
+	this.balls = [];
+	this.cannonHeight = this.radius / 2;
+	this.cannonWidth = this.cannonHeight * 3;
+}
+Cannon.prototype.draw = function (context) {
+	context.beginPath();
+	context.fillStyle = "blue";
+	context.arc(this.cx, this.cy, this.radius, 0, Math.PI * 2, true);
+	context.closePath();
+	context.fill();
+
+	context.save();
+	context.translate(this.cx, this.cy);
+	context.rotate(this.angle);
+	context.beginPath();
+	context.fillStyle = "red";
+	context.rect(0, -this.cannonHeight / 2, this.cannonWidth, this.cannonHeight);
+	context.closePath();
+	context.fill();
+	context.restore();
+
+	context.beginPath();
+	context.fillStyle = "yellow";
+	context.arc(this.cx, this.cy, this.radius / 2, 0, Math.PI * 2, true);
+	context.closePath();
+	context.fill();
+
+	for (var i = 0; i < this.balls.length; i++) {
+		this.balls[i].draw(context);
+	}
+}
+
+Cannon.prototype.update = function (map) {
+	if (!map.contain(this.cx, this.cy + this.radius))
+		this.cy += this.speed;
+	for (var i = 0; i < this.balls.length; i++) {
+		var item = this.balls[i];
+		var x = Math.floor(item.cx);
+		var y = Math.floor(item.cy);
+		if (item.update() || map.collide(x, y)) {
+			this.balls.splice(i, 1);
+		}
+	}
+}
+
+Cannon.prototype.fire = function (targetX, targetY) {
+	var dx = targetX - this.cx;
+	var dy = targetY - this.cy;
+	var power = Math.floor(Math.sqrt(dx * dx + dy * dy) / 20);
+
+	if (this.balls.length > 5)
+		return;
+	var dirX = Math.cos(this.angle);
+	var dirY = Math.sin(this.angle);
+
+	var startX = this.cx + this.cannonWidth * dirX;
+	var startY = this.cy + this.cannonWidth * dirY;
+
+	var ball = new Ball(this.mapWidth, this.mapHeight, startX, startY, dirX, dirY, power);
+	this.balls.push(ball);
+}
+Cannon.prototype.rotate = function (targetX, targetY) {
+	var dx = targetX - this.cx;
+	var dy = targetY - this.cy;
+	this.angle = Math.atan2(dy, dx);
+}
